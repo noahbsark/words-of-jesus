b76ac59a215671d238eb3479d1a5efd51dc39cd5
noahbsark <53667328+noahbsark@users.noreply.github.com>
2026-08-14 16:14:10 -0500

    Prepare GA4 measurement ID setting

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.SITE_CONFIG = {
   adsensePublisher: "ca-pub-6136525087204092",
   adSlots: { hero: "", inContent: "", footer: "" },
+  googleAnalyticsMeasurementId: "",
   amazonTag: "wordofjesus-20"
 };
