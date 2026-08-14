7b936b9a4d0a98b1fe1ce4648798c6949116d954
noahbsark <53667328+noahbsark@users.noreply.github.com>
2026-08-14 16:21:13 -0500

    Enable Google Analytics GA4

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SITE_CONFIG = {
   adsensePublisher: "ca-pub-6136525087204092",
   adSlots: { hero: "", inContent: "", footer: "" },
-  googleAnalyticsMeasurementId: "",
+  googleAnalyticsMeasurementId: "G-E8MP3D7BKJ",
   amazonTag: "wordofjesus-20"
 };
